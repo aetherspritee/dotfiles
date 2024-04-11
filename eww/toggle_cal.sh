c379bfbff1deee35956a2f72b7df9b228bf8cc32
aetherspritee <schautendustin@gmail.com>
2024-04-11 19:02:59 +0200

    calendar new api

--- eww/toggle_cal.sh
+++ eww/toggle_cal.sh
@@ -1,7 +1,7 @@
 eww_path="$PWD/"
-winstate=$(eww windows -c "$eww_path" | grep calendar)
+winstate=$(eww active-windows -c "$eww_path" | grep calendar)
 
-if [ "${winstate:0:1}" = "*" ]; then
+if [ "${winstate:0:1}" = "c" ]; then
     eww close calendar -c "$eww_path"
 else
     eww open calendar -c "$eww_path"
--- eww/toggle_lol.sh
+++ eww/toggle_lol.sh
@@ -1,6 +1,5 @@
 eww_path="$PWD/"
 winstate=$(eww active-windows | grep lolpopup)
-echo ${winstate:0:1}
 
 if [ "${winstate:0:1}" = "l" ]; then
     eww close lolpopup -c "$eww_path"
